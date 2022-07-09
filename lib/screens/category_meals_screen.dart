d18e177df5a6daead172360bf93d3637bfbe6c67
Nik Uzair <nikuzairsc@gmail.com>
2022-07-09 17:59:37 +0800

    add filter & sorting button

--- lib/screens/category_meals_screen.dart
+++ lib/screens/category_meals_screen.dart
@@ -15,6 +15,37 @@ class CategoryMealsScreen extends StatefulWidget {
   State<CategoryMealsScreen> createState() => _CategoryMealsScreenState();
 }
 
+Widget _buildInkWell(String title, Icon icon) {
+  return InkWell(
+    onTap: () {
+      print('hello');
+    },
+
+    child: Container(
+      width: 150,
+      height: 50,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          icon,
+          SizedBox(width: 10),
+          Text(
+            title,
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 18,
+            ),
+          ),
+        ],
+      ),
+      decoration: BoxDecoration(
+        color: Color.fromRGBO(1, 115, 112, 1.0),
+        borderRadius: BorderRadius.circular(15),
+      ),
+    ),
+  );
+}
+
 class _CategoryMealsScreenState extends State<CategoryMealsScreen> {
   String categoryTitle;
   List<Meal> displayedMeals;
@@ -42,40 +73,61 @@ class _CategoryMealsScreenState extends State<CategoryMealsScreen> {
     super.didChangeDependencies();
   }
 
-  void _removeMeal(String mealID) {
-    setState(
-      () {
-        displayedMeals.removeWhere((meal) => meal.id == mealID);
-      },
-    );
-  }
+  // void _removeMeal(String mealID) {
+  //   setState(
+  //     () {
+  //       displayedMeals.removeWhere((meal) => meal.id == mealID);
+  //     },
+  //   );
+  // }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(categoryTitle),
-        actions: [
-          Padding(
-            padding: EdgeInsets.all(15),
-            child: Icon(
-              Icons.filter_alt,
+      body: CustomScrollView(
+        slivers: <Widget>[
+          SliverAppBar(
+            expandedHeight: 90.0,
+            flexibleSpace: FlexibleSpaceBar(
+              title: Text(
+                categoryTitle,
+                style: TextStyle(color: Colors.black),
+              ),
+              centerTitle: true,
             ),
+            floating: true,
+            backgroundColor: Color.fromRGBO(247, 243, 239, 1),
           ),
+          SliverToBoxAdapter(
+            child: Column(
+              children: [
+                SizedBox(height: 10),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [_buildInkWell('Filter',Icon(Icons.filter_alt, color: Colors.white)), SizedBox(width: 20), _buildInkWell('Sorting',Icon(Icons.sort, color: Colors.white))],
+                ),
+                Container(
+                  child: ListView.builder(
+                    padding: EdgeInsets.all(8),
+                    shrinkWrap: true,
+                    physics: const NeverScrollableScrollPhysics(),
+                    itemCount: displayedMeals.length,
+                    itemBuilder: (ctx, index) {
+                      return MealItem(
+                        id: displayedMeals[index].id,
+                        title: displayedMeals[index].title,
+                        imageURL: displayedMeals[index].imageUrl,
+                        duration: displayedMeals[index].duration,
+                        complexity: displayedMeals[index].complexity,
+                        affordability: displayedMeals[index].affordability,
+                      );
+                    },
+                  ),
+                ),
+              ],
+            ),
+          )
         ],
-      ),
-      body: ListView.builder(
-        itemBuilder: (ctx, index) {
-          return MealItem(
-            id: displayedMeals[index].id,
-            title: displayedMeals[index].title,
-            imageURL: displayedMeals[index].imageUrl,
-            duration: displayedMeals[index].duration,
-            complexity: displayedMeals[index].complexity,
-            affordability: displayedMeals[index].affordability,
-          );
-        },
-        itemCount: displayedMeals.length,
       ),
     );
   }
